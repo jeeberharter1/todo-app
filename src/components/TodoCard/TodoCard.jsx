6a6a4c427d39c29e2dd980a107c33c762880e82f
jeeberharter1 <jeeberharter1@gmail.com>
2024-04-06 11:53:01 -0500

    fix complete todo click

--- src/components/TodoCard/TodoCard.jsx
+++ src/components/TodoCard/TodoCard.jsx
@@ -3,17 +3,14 @@ import './TodoCard.scss';
 const TodoCard = ({ todo, handleDelete, handleComplete }) => {
   return (
     <div className="todo-card">
-      <div 
-        className={todo.completed ? "check__container checked" : "check__container"} 
-        onClick={() => handleComplete(todo.id)}
-      >
-        {todo.completed && <img src='icon-check.svg' alt='X' />}
-      </div>
-      <div className="content__container">
-        <span className={todo.completed ? "strike" : ""}>{todo.title}</span>
-        <div className='cross__container' onClick={() => handleDelete(todo.id)} >
-          <img src="icon-cross.svg" alt="Delete" />
+      <div className="content__container" onClick={() => handleComplete(todo.id)}>
+        <div className={todo.completed ? "check__container checked" : "check__container"} >
+          {todo.completed && <img src='icon-check.svg' alt='X' />}
         </div>
+        <span className={todo.completed ? "strike" : ""}>{todo.title}</span>
+      </div>
+      <div className='cross__container' onClick={() => handleDelete(todo.id)} >
+        <img src="icon-cross.svg" alt="Delete" />
       </div>
     </div>
   )
